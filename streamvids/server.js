673fb2c4a6c33ff397a33890dcfc0b06c7e4f8d1
manuelbiolatiri <manuellaide49@gmail.com>
2020-10-26 23:58:57 +0000

    cors

--- streamvids/server.js
+++ streamvids/server.js
@@ -41,6 +41,7 @@ const port = process.env.PORT || 3002;
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
   
